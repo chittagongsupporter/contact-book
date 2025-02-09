21fabd8557ad249c1a6e52c01ca011e4fcc3e1ca
Srivatsan <talkads123@gmail.com>
2025-02-09 19:35:58 +0000

    create contact objects and modify arguments passed to contactbook member functions

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,10 +1,12 @@
 #include "ContactBook.hpp"
 #include <iostream>
 int main() {
+  Contact contact1("John Doe", "123-456-7890");
+  Contact contact2("Jane Smith", "987-545-5343");
   ContactBook contactBook1;
-  contactBook1.addContact(1);
-  contactBook1.addContact(2);
-  contactBook1.removeContact(2);
+  contactBook1.addContact(contact1);
+  contactBook1.addContact(contact2);
+  contactBook1.removeContact(contact2);
   contactBook1.viewContacts();
 
   return 0;
