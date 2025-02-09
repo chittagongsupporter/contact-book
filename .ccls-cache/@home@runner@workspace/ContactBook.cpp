d42f8902af2c0f16c16a03b634da9b1f264dd0f2
Srivatsan <talkads123@gmail.com>
2025-02-09 19:45:40 +0000

    refactor

--- .ccls-cache/@home@runner@workspace/ContactBook.cpp
+++ .ccls-cache/@home@runner@workspace/ContactBook.cpp
@@ -1,18 +1,19 @@
 #include "ContactBook.hpp"
 
+// Constructor
 ContactBook::ContactBook() {}
+
+// Member functions
 void ContactBook::addContact(Contact &contact) { contacts.push_back(contact); }
+
 void ContactBook::removeContact(Contact &contact) {
-  for (int i = 0; i < contacts.size(); i++) {
-    if (contacts[i].getName() == contact.getName()) {
+  for (int i = 0; i < contacts.size(); i++)
+    if (contacts[i].getName() == contact.getName())
       contacts.erase(contacts.begin() + i);
-    }
-  }
 }
 
 void ContactBook::viewContacts() const {
-  for (int i = 0; i < contacts.size(); i++) {
+  for (int i = 0; i < contacts.size(); i++)
     std::cout << contacts[i].getName() << " " << contacts[i].getPhoneNumber()
               << std::endl;
-  }
 }
