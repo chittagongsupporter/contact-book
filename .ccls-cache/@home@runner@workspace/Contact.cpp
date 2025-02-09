7854bdbda2c92f5321a05c162b09c1f4ac762895
Srivatsan <talkads123@gmail.com>
2025-02-09 19:31:13 +0000

    implement contact member functions

--- .ccls-cache/@home@runner@workspace/Contact.cpp
+++ .ccls-cache/@home@runner@workspace/Contact.cpp
@@ -5,3 +5,10 @@ Contact::Contact(std::string name, std::string phoneNumber) {
   this->name = name;
   this->phoneNumber = phoneNumber;
 }
+
+void Contact::setName(std::string name) { this->name = name; }
+void Contact::setPhoneNumber(std::string phoneNumber) {
+  this->phoneNumber = phoneNumber;
+}
+std::string Contact::getName() const { return this->name; }
+std::string Contact::getPhoneNumber() const { return this->phoneNumber; }
