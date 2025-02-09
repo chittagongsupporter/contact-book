c358aa4abcfc24fa28b80480b35c7c17f3acda37
Srivatsan <talkads123@gmail.com>
2025-02-09 19:41:14 +0000

    refactor for readability

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,13 +1,20 @@
 #include "ContactBook.hpp"
 #include <iostream>
 int main() {
+  // create contact objects
   Contact contact1("John Doe", "123-456-7890");
   Contact contact2("Jane Smith", "987-545-5343");
+
+  // create contact book object
   ContactBook contactBook1;
+
+  // add contacts to contact book
   contactBook1.addContact(contact1);
   contactBook1.addContact(contact2);
-  contactBook1.removeContact(contact2);
-  contactBook1.viewContacts();
+
+  contactBook1.removeContact(contact2); // remove
+
+  contactBook1.viewContacts(); // view all contacts
 
   return 0;
 }
