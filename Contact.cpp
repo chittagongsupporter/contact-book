712511d7e92c078f596ae67c96ff5aa2820e1a6b
Srivatsan <talkads123@gmail.com>
2025-02-09 19:42:30 +0000

    refactor

--- Contact.cpp
+++ Contact.cpp
@@ -1,14 +1,18 @@
 #include "Contact.hpp"
 
+// Constructors
 Contact::Contact() : Contact("", "") {}
 Contact::Contact(std::string name, std::string phoneNumber) {
   this->name = name;
   this->phoneNumber = phoneNumber;
 }
 
+// Setters
 void Contact::setName(std::string name) { this->name = name; }
 void Contact::setPhoneNumber(std::string phoneNumber) {
   this->phoneNumber = phoneNumber;
 }
+
+// Getters
 std::string Contact::getName() const { return this->name; }
 std::string Contact::getPhoneNumber() const { return this->phoneNumber; }
