c40a47f4098dda4d0bff56bd4de097b01516631d
Srivatsan <talkads123@gmail.com>
2025-02-09 18:23:22 +0000

    create Contact.hpp

---
+++ Contact.hpp
@@ -0,0 +1,20 @@
+#ifndef Contact_H
+#define Contact_H
+
+class Contact
+{
+public:
+  // data members
+  std::string name;
+  std::string phoneNumber;
+  // default constructor delegates to paramertized constructor
+  Contact()
+  // paramertized constructor
+  Contact(std::string name, std::string phoneNumber);
+
+  // member functions
+  void setName(std::string name);
+  void setPhoneNumber(std::string phoneNumber);
+  std::string getName();
+  std::string getPhoneNumber();
+};
