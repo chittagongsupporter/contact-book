4cd4a1385ecce4002c4a0ba3b93b0cbe292ca0ec
Srivatsan <talkads123@gmail.com>
2025-02-09 19:29:17 +0000

    modify member functions to support Contact type

--- .ccls-cache/@home@runner@workspace/ContactBook.cpp
+++ .ccls-cache/@home@runner@workspace/ContactBook.cpp
@@ -1,18 +1,18 @@
 #include "ContactBook.hpp"
 
 ContactBook::ContactBook() {}
-void ContactBook::addContact(int contact) { contacts.push_back(contact); }
-void ContactBook::removeContact(int contact) {
-  for (auto it = contacts.begin(); it != contacts.end(); ++it) {
-    if (*it == contact) {
-      contacts.erase(it);
-      return;
+void ContactBook::addContact(Contact &contact) { contacts.push_back(contact); }
+void ContactBook::removeContact(Contact &contact) {
+  for (int i = 0; i < contacts.size(); i++) {
+    if (contacts[i].getName() == contact.getName()) {
+      contacts.erase(contacts.begin() + i);
     }
   }
 }
 
-void ContactBook::viewContacts() {
-  for (auto it = contacts.begin(); it != contacts.end(); ++it) {
-    std::cout << *it << std::endl;
+void ContactBook::viewContacts() const {
+  for (int i = 0; i < contacts.size(); i++) {
+    std::cout << contacts[i].getName() << " " << contacts[i].getPhoneNumber()
+              << std::endl;
   }
 }
